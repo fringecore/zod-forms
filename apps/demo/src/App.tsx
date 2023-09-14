83a1891777eacd00e26c6e16ad0ea8184be98c26
afiatamanna06 <afiatamanna06@gmail.com>
2023-09-14 11:49:54 +0600

    Fix minor issues

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,19 +1,20 @@
-import {useZodForm} from 'zod-forms';
+import {useZodForm} from '../../../packages/zod-forms/src';
 import z from 'zod';
 
 const schema = z.object({
+    name: z.string(),
     address: z.object({
         is_permanent: z.boolean(),
     }),
 });
 
 function App() {
-    const form = useZodForm(schema);
+    const { form } = useZodForm(schema);
 
     return (
         <>
             <form.fields.address.is_permanent.Input>
-                {({value, onChange}) => {
+                {({value, onChange}: {value: boolean, onChange: (value: boolean) => void}) => {
                     return (
                         <div onClick={() => onChange(!value)}>
                             {value ? <>✅</> : <>☐</>}
@@ -21,7 +22,9 @@ function App() {
                     );
                 }}
             </form.fields.address.is_permanent.Input>
-            
+            <form.fields.name.Input>
+                
+            </form.fields.name.Input>
         </>
     );
 }
