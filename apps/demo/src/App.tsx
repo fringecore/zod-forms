14215d94d9c10d610cead4b137f8bb71bf7c364d
Omran Jamal <o.jamal97@gmail.com>
2023-10-01 15:39:56 +0600

    sample usage

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -1,28 +1,94 @@
-import {useZodForm} from 'zod-forms';
 import z from 'zod';
+import {useZodForm} from 'zod-forms';
+import {useEffect} from 'react';
 
 const schema = z.object({
-    address: z.object({
-        is_permanent: z.boolean(),
+    name: z.object({
+        first: z.string(),
+        middle: z.string().optional(),
+        last: z.string(),
     }),
+    age: z.number().min(18).max(150),
+    favoriteColor: z.enum(['red', 'blue', 'green']),
+    favoriteFoods: z.array(z.string()),
+    isAwesome: z.boolean(),
 });
 
-function App() {
-    const form = useZodForm(schema);
+function NameForm() {
+    const {from, useFormData} = useZodForm(schema.shape.name);
+    const data = useFormData();
+
+    useEffect(() => {
+        console.log(data.first);
+    }, [data]);
 
     return (
         <>
-            <form.fields.address.is_permanent.Input>
-                {({value, onChange}) => {
+            <form.first.Input>
+                {(value, onChange) => {
                     return (
-                        <div onClick={() => onChange(!value)}>
-                            {value ? <>✅</> : <>☐</>}
-                        </div>
+                        <input
+                            type={'text'}
+                            value={value}
+                            onChange={(ev) => onChange(ev.target.value)}
+                        />
                     );
                 }}
-            </form.fields.address.is_permanent.Input>
+            </form.first.Input>
+            <form.middle.Input>
+                {(value, onChange) => {
+                    return (
+                        <input
+                            type={'text'}
+                            value={value}
+                            onChange={(ev) => onChange(ev.target.value)}
+                        />
+                    );
+                }}
+            </form.middle.Input>
+            <form.last.Input>
+                {(value, onChange) => {
+                    return (
+                        <input
+                            type={'text'}
+                            value={value}
+                            onChange={(ev) => onChange(ev.target.value)}
+                        />
+                    );
+                }}
+            </form.last.Input>
         </>
     );
+}
+
+function MainForm() {
+    const {form, useFormData} = useZodForm(schema);
+    const data = useFormData();
+
+    useEffect(() => {
+        console.log(data.name.first, data.name.middle, data.name.last);
+    }, [data.name]);
+
+    return (
+        <>
+            <NameForm />
+            <form.age.Input>
+                {(value, onChange) => {
+                    return (
+                        <input
+                            type={'number'}
+                            value={value}
+                            onChange={(ev) => onChange(ev.target.value)}
+                        />
+                    );
+                }}
+            </form.age.Input>
+        </>
+    );
+}
+
+function App() {
+    return <MainForm />;
 }
 
 export default App;
