9eb6bc362bab2fb9ee0fbd7bdd7cf04b53a409c5
Omran Jamal <o.jamal97@gmail.com>
2023-10-04 04:40:45 +0600

    simplified export structure

--- packages/zod-forms/src/index.tsx
+++ packages/zod-forms/src/index.tsx
@@ -1,12 +1,7 @@
 export * from './useZodForm';
+export * from './useFormData';
 
-export {Emitter, createEmitter} from './utils/emitter';
-export {DataSymbol, EmitterSymbol, EmittersSymbol} from './symbols';
-export {FormEmittersType} from './types/CoreTypes';
-export {FormFieldsCacheType} from './types/CoreTypes';
-export {RootFieldsType} from './types/CoreTypes';
-export {ZodFormFieldType} from './types/CoreTypes';
-export {RootSymbolFields} from './types/CoreTypes';
-export {TerminateFieldType} from './types/CoreTypes';
-export {useFormData} from './useFormData';
-export {ContextType} from './types/CoreTypes';
+export * from './utils/emitter';
+export * from './symbols';
+
+export * from './types/CoreTypes';
