9b37efd1970ae2deafa0db7c5786b1bfdf45d5b7
afiatamanna06 <afiatamanna06@gmail.com>
2023-10-05 17:37:25 +0600

    Remove redundancy

--- packages/zod-forms/src/inputs/ArrayInput.tsx
+++ packages/zod-forms/src/inputs/ArrayInput.tsx
@@ -5,30 +5,6 @@ import {ArrayFieldComponentType} from '../types/AllFieldTypes';
 import {ZodObject} from 'zod';
 import {ContextType} from '../types/CoreTypes';
 
-/*export function ArrayInput<
-    SCHEMA_TYPE extends ZodObject<any>,
-    VALUE extends string,
->({
-    context,
-    leafPath,
-    component: Component,
-}: {
-    context: ContextType<SCHEMA_TYPE>;
-    leafPath: [string, ...string[]];
-    component: ArrayFieldComponentType<VALUE>;
-}) {
-    const values =
-        useValue<VALUE[], any>(context.emitters, context.data, leafPath) ?? [];
-
-    const onChange = useOnChange<VALUE[], any>(
-        context.emitters,
-        context.data,
-        leafPath,
-    );
-
-    return <Component values={values} onChange={onChange} />;
-}*/
-
 export function ArrayInput<
     SCHEMA_TYPE extends ZodObject<any>,
     VALUE extends string | '',
